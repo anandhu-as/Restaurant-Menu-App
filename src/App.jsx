aa7092be463a00de81351c29b655a08c74c42157
Anandhu as <109361830+anandhu-as@users.noreply.github.com>
2023-05-20 23:07:55 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,23 @@ import items from "./data";
 import "./styles/App.css";
 const App = () => {
   const [menuItems, setMenuItems] = useState(items);
-  const categories = ['all',...new Set(items.map(item => item['category']))]
+  const categories = ["all", ...new Set(items.map((item) => item["category"]))];
   const filterItems = (category) => {
     category === "all"
       ? setMenuItems(items)
       : setMenuItems(() => items.filter((item) => item.category === category));
   };
   return (
-    
     <main>
-         <nav><h1>Restaurant-Menu</h1></nav>
+      <nav>
+        <h1>Restaurant-Menu</h1>
+      </nav>
       <section className="menu section">
         <div className="title">
           <h2>our menu</h2>
           <div className="underline"></div>
         </div>
-        <Categories filterItems={filterItems}  categories={categories}/>
+        <Categories filterItems={filterItems} categories={categories} />
         <Menu items={menuItems} />
       </section>
     </main>
